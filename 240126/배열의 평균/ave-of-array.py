442e1a68b87c7d04e43c97bdfdd20bd3ebb8b537
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-01-26 22:58:41 +0900

    Title: 배열의 평균, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/배열의 평균/ave-of-array.py
+++ 240126/배열의 평균/ave-of-array.py
@@ -5,14 +5,14 @@ def avg(x):
 for _ in range(2):
     lst.append(list(map(int, input().split())))
 #row avg
-print(avg(lst[0]), avg(lst[1]))
+print(round(avg(lst[0]),1), round(avg(lst[1]),1))
 
 
 lst_t = [list(x) for x in zip(*lst)]
 #col avg
 col_avg = ''
 for col in lst_t:
-    col_avg = col_avg + str(avg(col)) + " "
+    col_avg = col_avg + str(round(avg(col),1)) + " "
 print(col_avg)
 #total avg
 length = 0
@@ -20,4 +20,4 @@ total_sum = 0
 for i in lst:
     total_sum = total_sum + sum(i)
     length = length + len(i)
-print(total_sum/length)
+print(round(total_sum/length, 1))
