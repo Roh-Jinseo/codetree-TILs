b84e416acabb75db5f83d5abeac90ca8e949a2f5
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-01-28 01:19:58 +0900

    Title: 대각선으로 숫자 채우기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,52 @@
+#input
+n, m = map(int, input().split())
+
+a = [
+    [ 0 for _ in range(m)]
+    for _ in range(n)
+]
+
+#
+
+a[0][0] = 1
+num = 2
+#pt1
+for cross in range(2, min(n, m)):
+    row = 0
+    col = cross - 1 
+    for _ in range(cross):
+        a[row][col] = num
+        col -= 1
+        row += 1
+        num += 1
+#pt2
+for col_start in range(min(n, m), max(n, m)):
+    cross_len = min(n, m)
+    row = 0
+    col = col_start - 1
+    for _ in range(cross_len):
+        a[row][col] = num
+        col -= 1
+        row += 1
+        num += 1
+#pt3
+for cross_len in range(n , 0, -1):
+#    print(n, cross_len)
+    row = n - cross_len
+    col = m - 1
+    
+    for _ in range(cross_len):
+        a[row][col] = num
+#        print(row, col, a[row][col])
+        col -= 1
+        row += 1
+        num += 1
+
+        
+
+#result
+for row in a:
+    for i in row:
+        print(i, end=" ")
+    print()
+#print(arr)
