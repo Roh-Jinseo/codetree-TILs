6b1018f321b0422dcc6e92779f606a37bcf0c11a
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-03-20 00:05:34 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 522ms, Memory: 29MB, Status: Passed - CodeTree

--- 240320/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240320/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,3 +1,15 @@
+def perm(lev):
+    if lev == M :
+        print(*path)
+        return
+    
+    for i in range(N):
+        path.append(arr[i])
+        perm(lev+1)
+        path.pop()
+
 N, M = map(int, input().split())
-arr = list(range(1, N))
-print(arr)
+arr = list(range(1, N+1))
+path=[]
+
+perm(0)
