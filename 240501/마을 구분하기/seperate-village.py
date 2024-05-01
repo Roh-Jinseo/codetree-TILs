2173a2fe52ffa93a864ddabd5049ca1e2d2174c4
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-05-01 16:04:37 +0900

    Title: 마을 구분하기, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/마을 구분하기/seperate-village.py
@@ -0,0 +1,30 @@
+dx=[0,1]
+dy=[1,0]
+
+def dfs(v):
+    global cnt
+    x,y = v
+    for k in range(2):
+        nx=x+dx[k]
+        ny=y+dy[k]
+        if 0<=nx<N and 0<=ny<N and not visited[nx][ny] and arr[nx][ny]:
+            cnt += 1
+            visited[nx][ny] = 1
+            dfs((nx,ny))
+
+
+N=int(input())
+arr=[ list(map(int, input().split())) for _ in range(N) ]
+visited=[ [0]*N for _ in range(N) ]
+ans = []
+for i in range(N):
+    for j in range(N):
+        cnt = 1
+        if not visited[i][j] and arr[i][j] : 
+            visited[i][j] = 1
+            dfs((i,j))
+            if cnt > 0 : ans.append(cnt)
+print(len(ans))
+ans.sort()
+for a in ans:
+    print(a)
