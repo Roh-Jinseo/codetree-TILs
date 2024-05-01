656ae2faf2438afb19066493d56fd5e47ae6d18a
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-05-01 15:41:08 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 145ms, Memory: 27MB, Status: Passed - CodeTree

--- 240501/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240501/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,21 +1,33 @@
-dx=[0,1]
-dy=[1,0]
+dx = [0, 1]
+dy = [1, 0]
+
 
 def dfs(v):
+    global flag
+    if flag :
+        return
     x, y = v
     for k in range(2):
         nx = x + dx[k]
-        ny = y + dy[k] 
-        if 0<=nx<N and 0<=ny<M and not visited[x][y] and arr[x][y]:
+        ny = y + dy[k]
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and arr[x][y]:
             visited[nx][ny] = 1
-            dfs((nx,ny))
-            if nx == N-1 and ny == M -1 : return 1
-            
+            if nx == N - 1 and ny == M - 1:
+                # print("goal")
+                flag=True
+                return 1
+            dfs((nx, ny))
+
     return 0
 
-N, M = map(int,input().split())
-arr = [ list(map(int,input().split())) for _ in range(N) ]
-end = (N-1,M-1)
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+end = (N - 1, M - 1)
 path = []
-visited = [ [0]*M for _ in range(N) ]
-print(dfs((0,0)))
+visited = [[0] * M for _ in range(N)]
+visited[0][0]=1
+flag=False
+dfs((0, 0))
+print(int(flag))
+# print(visited)
