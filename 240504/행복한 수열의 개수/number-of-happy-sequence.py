5ae2458795c9d2f1a7dd36ea4879bb10df3b0d18
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-05-04 20:03:52 +0900

    Title: 행복한 수열의 개수, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240504/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240504/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,8 +1,17 @@
+# import sys; sys.stdin = open("행복수열_input.txt")
+"""
+4 3
+7 1 8 3
+6 1 8 7
+7 7 7 7
+8 1 9 7
+"""
 N, M = map(int, input().split())
 arr = [ list(map(int, input().split())) for _ in range(N) ]
 
 happy = 0
-for i in range(N): #가로 
+for i in range(N): #가로
+    is_happy=False
     for j in range(N-M+1):
         maxi = 0
         cnt = 0
@@ -14,33 +23,35 @@ for i in range(N): #가로
                 maxi = max(cnt, maxi)
                 cnt = 0
             # print(arr[i][j+k],cnt)
-            maxi = max(cnt, maxi)
+        maxi = max(cnt, maxi)
         if maxi >= M :
             happy += 1
-            # print(arr[i][j], maxi,happy)
+            # print((i,j),arr[i][j], maxi,happy)
             break
 
 for i in range(N): #transpose
-    for j in range(N-M+1):
-        if i>j : arr[i][j], arr[j][i] = arr[j][i], arr[i][j] 
+    for j in range(N):
+        if i<j : arr[i][j], arr[j][i] = arr[j][i], arr[i][j]
 # print('---')
 # for a in arr:
 #     print(a)
-for i in range(N): #가로 
+for i in range(N): #세로
     for j in range(N-M+1):
         maxi = 0
         cnt = 0
         num = arr[i][j]
         for k in range(M):
+            # print((i,j+k), arr[i][j+k],end=" ")
             if arr[i][j+k] == num:
                 cnt += 1
             else:
                 maxi = max(cnt, maxi)
                 cnt = 0
             # print(arr[i][j+k],cnt)
-            maxi = max(cnt, maxi)
+        maxi = max(cnt, maxi)
         if maxi >= M :
             happy += 1
-            # print(arr[i][j], maxi,happy)
-        break
+            # print((i,j),arr[i][j], maxi,happy)
+            break
+        # print()
 print(happy)
